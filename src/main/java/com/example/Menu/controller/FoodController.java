1777fd6eeea35124fb772c43e59f9c5ca6646a83
jose <josefilmeneses@gmail.com>
2023-05-08 21:43:40 +0100

    added the class food

--- src/main/java/com/example/Menu/controller/FoodController.java
+++ src/main/java/com/example/Menu/controller/FoodController.java
@@ -1,7 +1,15 @@
 package com.example.Menu.controller;
 
+import com.example.Menu.Food.Food;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("food")
 public class FoodController {
+@GetMapping
+    public void getAll() {
+
+    }
 }
